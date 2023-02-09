549869fa062aafcce3dbb47025519f2ec3681a6f
ALGUL1987 <tankipanki1@gmail.com>
2023-02-09 20:22:04 +0300

    Example003_Sum - random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int A = 10;
-int B = 13;
+﻿int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
 int sum = A + B;
+Console.WriteLine(A);
+Console.WriteLine(B);
 Console.WriteLine(sum);
